b12d7b1b4b5d72cdb5be36709140c4e0f124b460
calico-g <calico.goodrich@gmail.com>
2013-03-08 15:06:59 -0800

    passed create gig spec

--- app/controllers/gigs_controller.rb
+++ app/controllers/gigs_controller.rb
@@ -2,8 +2,25 @@ class GigsController < ApplicationController
 
 	def index
 	end
+
 	def new
 		@gig = Gig.new
+	end
+
+	def create
+		@gig = Gig.new(params[:gig])
+
+		if @gig.save
+			flash[:notice] = "Gig saved!"
+			redirect_to @gig
+		else
+			flash[:alert] = "Gig NOT saved."
+			render :action => "new"
+		end
+	end
+
+	def show
+
 	end
 
 end
