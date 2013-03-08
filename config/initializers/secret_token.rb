d78fdd947df964ffb9570d16bd9e6de5664b1167
calico-g <calico.goodrich@gmail.com>
2013-03-08 10:30:39 -0800

    Add rails structure.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Temp::Application.config.secret_token = '840d65a720deb4da5440b3abb81be598d6551a2cd5a508e4b1ee5c68ac16c58aba3f74224ead5b115e0605a82f36b4bf3664888dabd3e026d5ed70db60a2c925'
