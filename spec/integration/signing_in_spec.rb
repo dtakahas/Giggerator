f9cebe362b540448a0e77b54521634a6e88f5775
calico-g <calico.goodrich@gmail.com>
2013-03-13 15:01:48 -0700

    add user authentication from form

--- spec/integration/signing_in_spec.rb
+++ spec/integration/signing_in_spec.rb
@@ -13,4 +13,14 @@ feature "Signing in" do
 		page.should have_content("Your account was successfully confirmed")
 		page.should have_content("Signed in as user@example.com")
 	end
+
+	scenario "Signing in via form" do
+		User.find_by_email('user@example.com').confirm!
+		visit '/'
+		click_link "Sign in"
+		fill_in "Email", :with => "user@example.com"
+		fill_in "Password", :with => "password"
+		click_button "Sign in"
+		page.should have_content "Signed in successfully."
+	end
 end
