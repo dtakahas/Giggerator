9bf9960c98777b95aa62dbbb3b27f14e780e0c6e
calico-g <calico.goodrich@gmail.com>
2013-03-24 13:07:18 -0700

    Contacts cannot be created without a name

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,6 @@ class Contact < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name, :notes, :phone, :user_id
   belongs_to :user
   has_and_belongs_to_many :gig
+  validates :first_name, :presence => true
+
 end
--- spec/integration/creating_contacts_spec.rb
+++ spec/integration/creating_contacts_spec.rb
@@ -38,6 +38,7 @@ feature "Creating contacts" do
     page.should have_content "Pope John Paul"
   end
 
+  # IS THIS LEFT OVER FROM THAT OTHER IDEA?
   # scenario "can create contact from gig show" do
   #   sign_in_as!(user)
   #   click_link "Jazz Bash at the Casbah"
@@ -48,12 +49,13 @@ feature "Creating contacts" do
   #   page.should have_content("Contact saved!")
   # end
 
-  # scenario "cannot create a contact without a name" do
-  #   sign_in_as!(user)
-  #   visit '/'
-  #   click_link "New Contact"
-  #   click_button "Save Contact"
-  #   page.should have_content("Please enter a name!")
-  # end
+  scenario "cannot create a contact without a name" do
+    sign_in_as!(user)
+    visit '/'
+    click_link "Contacts"
+    click_link "New Contact"
+    click_button "Save Contact"
+    page.should have_content("First name can't be blank")
+  end
 
 end
