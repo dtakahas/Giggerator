1cb3d163f41030d16a1395d5d8bfde6ba02df41a
dtakahas <dtcello@gmail.com>
2013-03-26 14:34:51 -0700

    Remove extra files

--- db/migrate/20130311212722_add_contacts_to_gigs.rb
+++
@@ -1,7 +0,0 @@
-class AddContactsToGigs < ActiveRecord::Migration
-  def change
-    add_column :gigs, :contact_name, :string
-    add_column :gigs, :contact_phone, :string
-    add_column :gigs, :contact_email, :string
-  end
-end
