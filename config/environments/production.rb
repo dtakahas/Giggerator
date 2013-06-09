61303b840776aa98e4c8127b2ec9e9e9c219ba81
dtakahas <dtcello@gmail.com>
2013-06-08 18:13:19 -0700

    Configure devise mailing for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,9 @@ Temp::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  # For devise gem
+  config.action_mailer.default_url_options = { :host => 'localhost' }
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
